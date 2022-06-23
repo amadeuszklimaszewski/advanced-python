7b2b78e14af5c44570e56f4c2380a11a6c7a5a67
Amadeusz Klimaszewski <ami0126538815@gmail.com>
2022-06-23 21:07:40 +0200

    Adapter, bridge and composite design patterns

---
+++ Design_patterns/structural_design_patterns/adapter.py
@@ -0,0 +1,43 @@
+# Adapter method helps us in making the incompatible objects adaptable to each other.
+# The Adapter method is one of the easiest methods to understand because we have a lot
+# of real-life examples that show the analogy with it. The main purpose of this method
+# is to create a bridge between two incompatible interfaces. This method provides a
+# different interface for a class. We can more easily understand the concept by thinking
+# about the Cable Adapter that allows us to charge a phone somewhere that has outlets in
+# different shapes. Using this idea, we can integrate the classes that couldn’t be
+# integrated due to interface incompatibility.
+
+
+class Target:
+    def request(self) -> str:
+        return "Target: The default target's behavior."
+
+
+class Adaptee:
+    def client_incompatible_request(self) -> str:
+        return "client the to behaviour Unknown"
+
+
+class Adapter(Target, Adaptee):
+    def request(self) -> str:
+        result = self.client_incompatible_request()
+        translation = " ".join(list(result.split())[::-1])
+        return f"Adapter: (Translated) {translation}"
+
+
+def client_code(target: Target) -> None:
+    print(target.request(), end="")
+
+
+if __name__ == "__main__":
+    print("Client works fine with the Target objects:")
+    target = Target()
+    client_code(target)
+    print("\n")
+
+    adaptee = Adaptee()
+    print(f"Adaptee: {adaptee.client_incompatible_request()}", end="\n\n")
+
+    print("Client: But I can work with it via the Adapter:")
+    adapter = Adapter()
+    client_code(adapter)
---
+++ Design_patterns/structural_design_patterns/bridge.py
@@ -0,0 +1,54 @@
+#  Bridge is a structural design pattern that divides business logic or huge class
+# into separate class hierarchies that can be developed independently.
+
+
+from abc import ABC, abstractmethod
+
+
+class Implementation(ABC):
+    @abstractmethod
+    def operation_implementation(self) -> str:
+        pass
+
+
+class ConcreteImplementationA(Implementation):
+    def operation_implementation(self) -> str:
+        return "Concrete implementation A."
+
+
+class ConcreteImplementationB(Implementation):
+    def operation_implementation(self) -> str:
+        return "Concrete implementation B."
+
+
+class Abstraction:
+    def __init__(self, implementation: Implementation) -> None:
+        self.implementation = implementation
+
+    def operation(self) -> str:
+        return (
+            f"Abstraction: Base operation with:\n"
+            f"{self.implementation.operation_implementation()}"
+        )
+
+
+class ExtendedAbstraction(Abstraction):
+    def operation(self) -> str:
+        return (
+            f"ExtendedAbstraction: Extended operation with:\n"
+            f"{self.implementation.operation_implementation()}"
+        )
+
+
+def client_code(abstraction: Abstraction) -> None:
+    print(abstraction.operation())
+
+
+if __name__ == "__main__":
+    implementation = ConcreteImplementationA()
+    abstraction = Abstraction(implementation)
+    client_code(abstraction)
+
+    implementation2 = ConcreteImplementationB()
+    abstraction2 = ExtendedAbstraction(implementation2)
+    client_code(abstraction2)
