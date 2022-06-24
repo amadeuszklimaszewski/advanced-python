6cc567291d948ca9ab7538dfbb2f66498b0a1100
Amadeusz Klimaszewski <ami0126538815@gmail.com>
2022-06-24 20:21:50 +0200

    Mediator

---
+++ Design_patterns/behavioral_design_patterns/mediator.py
@@ -0,0 +1,79 @@
+# Mediator Method is a Behavioral Design Pattern that allows us to
+# reduce the unordered dependencies between the objects. In a mediator
+# environment, objects take the help of mediator objects to communicate
+# with each other. It reduces coupling by reducing the dependencies
+# between communicating objects. The mediator works as a router between
+# objects and it can have it’s own logic to provide a way of
+# communication.
+
+from __future__ import annotations
+from abc import ABC
+
+
+class Mediator(ABC):
+    def notify(self, sender: object, event: str) -> None:
+        pass
+
+
+class ConcreteMediator(Mediator):
+    def __init__(self, component1: Component1, component2: Component2) -> None:
+        self._component1 = component1
+        self._component1.mediator = self
+        self._component2 = component2
+        self._component2.mediator = self
+
+    def notify(self, sender: object, event: str) -> None:
+        if event == "A":
+            print("Mediator reacts on A and triggers following operations:")
+            self._component2.do_c()
+        elif event == "D":
+            print("Mediator reacts on D and triggers following operations:")
+            self._component1.do_b()
+            self._component2.do_c()
+
+
+class BaseComponent:
+    def __init__(self, mediator: Mediator = None) -> None:
+        self._mediator = mediator
+
+    @property
+    def mediator(self) -> Mediator:
+        return self._mediator
+
+    @mediator.setter
+    def mediator(self, mediator: Mediator) -> None:
+        self._mediator = mediator
+
+
+class Component1(BaseComponent):
+    def do_a(self) -> None:
+        print("Component 1 does A.")
+        self.mediator.notify(self, "A")
+
+    def do_b(self) -> None:
+        print("Component 1 does B.")
+        self.mediator.notify(self, "B")
+
+
+class Component2(BaseComponent):
+    def do_c(self) -> None:
+        print("Component 2 does C.")
+        self.mediator.notify(self, "C")
+
+    def do_d(self) -> None:
+        print("Component 2 does D.")
+        self.mediator.notify(self, "D")
+
+
+if __name__ == "__main__":
+    c1 = Component1()
+    c2 = Component2()
+    mediator = ConcreteMediator(c1, c2)
+
+    print("Client triggers operation A.")
+    c1.do_a()
+
+    print("\n", end="")
+
+    print("Client triggers operation D.")
+    c2.do_d()
