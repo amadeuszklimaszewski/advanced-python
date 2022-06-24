ba1b19f1385590d089d0588ff0d2058f131732a6
Amadeusz Klimaszewski <ami0126538815@gmail.com>
2022-06-24 22:37:33 +0200

    Memento

---
+++ Design_patterns/behavioral_design_patterns/memento.py
@@ -0,0 +1,113 @@
+# Memento Method is a Behavioral Design pattern which provides
+# the ability to restore an object to its previous state. Without
+# revealing the details of concrete implementations, it allows you
+# to save and restore the previous version of the object. It tries
+# not to disturb the encapsulation of the code and allows you to
+# capture and externalize an object’s internal state.
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from datetime import datetime
+from random import sample
+from string import ascii_letters
+
+
+class Originator:
+    _state = None
+
+    def __init__(self, state: str) -> None:
+        self._state = state
+        print(f"Originator: My initial state is: {self._state}")
+
+    def do_something(self) -> None:
+        print("Originator: I'm doing something important.")
+        self._state = self._generate_random_string(30)
+        print(f"Originator: and my state has changed to: {self._state}")
+
+    def _generate_random_string(self, length: int = 10) -> str:
+        return "".join(sample(ascii_letters, length))
+
+    def save(self) -> Memento:
+        return ConcreteMemento(self._state)
+
+    def restore(self, memento: Memento) -> None:
+        self._state = memento.get_state()
+        print(f"Originator: My state has changed to: {self._state}")
+
+
+class Memento(ABC):
+    @abstractmethod
+    def get_state(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_name(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_date(self) -> str:
+        pass
+
+
+class ConcreteMemento(Memento):
+    def __init__(self, state: str) -> None:
+        self._state = state
+        self._date = str(datetime.now())[:19]
+
+    def get_state(self) -> str:
+        return self._state
+
+    def get_name(self) -> str:
+        return f"{self._date} / ({self._state[0:9]}...)"
+
+    def get_date(self) -> str:
+        return self._date
+
+
+class Caretaker:
+    def __init__(self, originator: Originator) -> None:
+        self._mementos: list[Memento] = []
+        self._originator = originator
+
+    def backup(self) -> None:
+        print("\nCaretaker: Saving Originator's state...")
+        self._mementos.append(self._originator.save())
+
+    def undo(self) -> None:
+        if not len(self._mementos):
+            return
+
+        memento = self._mementos.pop()
+        print(f"Caretaker: Restoring state to: {memento.get_name()}")
+        try:
+            self._originator.restore(memento)
+        except Exception:
+            self.undo()
+
+    def show_history(self) -> None:
+        print("Caretaker: List of mementos:")
+        for memento in self._mementos:
+            print(memento.get_name())
+
+
+if __name__ == "__main__":
+    originator = Originator("State_state")
+    caretaker = Caretaker(originator)
+
+    caretaker.backup()
+    originator.do_something()
+
+    caretaker.backup()
+    originator.do_something()
+
+    caretaker.backup()
+    originator.do_something()
+
+    print()
+    caretaker.show_history()
+
+    print("\nClient: Doing rollback...\n")
+    caretaker.undo()
+
+    print("\nClient: Doing rollback again...\n")
+    caretaker.undo()
