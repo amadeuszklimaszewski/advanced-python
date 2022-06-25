a2969407d1f44ecb8a7037e0f007d3f17173b0e9
Amadeusz Klimaszewski <ami0126538815@gmail.com>
2022-06-25 19:43:54 +0200

    State

---
+++ Design_patterns/behavioral_design_patterns/state.py
@@ -0,0 +1,74 @@
+# State method is Behavioral Design Pattern that allows an object
+# to change its behavior when there occurs a change in its internal
+# state. It helps in implementing the state as a derived class of
+# the state pattern interface. If we have to change the behavior of
+# an object based on its state, we can have a state variable in the
+# Object and use if-else condition block to perform different actions
+# based on the state. It may be termed as the object-oriented state
+# machine. It implements the state transitions by invoking methods
+# from the pattern’s superclass.
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class Context:
+    _state = None
+
+    def __init__(self, state: State) -> None:
+        self.transition_to(state)
+
+    def transition_to(self, state: State):
+        print(f"Context: Transition to {type(state).__name__}")
+        self._state = state
+        self._state.context = self
+
+    def request1(self):
+        self._state.handle1()
+
+    def request2(self):
+        self._state.handle2()
+
+
+class State(ABC):
+    @property
+    def context(self) -> Context:
+        return self._context
+
+    @context.setter
+    def context(self, context: Context) -> None:
+        self._context = context
+
+    @abstractmethod
+    def handle1(self) -> None:
+        pass
+
+    @abstractmethod
+    def handle2(self) -> None:
+        pass
+
+
+class ConcreteStateA(State):
+    def handle1(self) -> None:
+        print("ConcreteStateA handles request1.")
+        print("ConcreteStateA wants to change the state of the context.")
+        self.context.transition_to(ConcreteStateB())
+
+    def handle2(self) -> None:
+        print("ConcreteStateA handles request2.")
+
+
+class ConcreteStateB(State):
+    def handle1(self) -> None:
+        print("ConcreteStateB handles request1.")
+
+    def handle2(self) -> None:
+        print("ConcreteStateB handles request2.")
+        print("ConcreteStateB wants to change the state of the context.")
+        self.context.transition_to(ConcreteStateA())
+
+
+if __name__ == "__main__":
+    context = Context(ConcreteStateA())
+    context.request1()
+    context.request2()
