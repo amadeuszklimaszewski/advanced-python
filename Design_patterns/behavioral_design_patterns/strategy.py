d562b0f059db00673d603be5fb34d7ef46837fd7
Amadeusz Klimaszewski <ami0126538815@gmail.com>
2022-06-25 20:20:36 +0200

    Strategy

---
+++ Design_patterns/behavioral_design_patterns/strategy.py
@@ -0,0 +1,57 @@
+# The strategy method is Behavioral Design pattern that allows
+# you to define the complete family of algorithms, encapsulates
+# each one and putting each of them into separate classes and also
+# allows to interchange there objects. It is implemented in Python
+# by dynamically replacing the content of a method defined inside
+# a class with the contents of functions defined outside of the
+# class. It enables selecting the algorithm at run-time. This
+# method is also called as Policy Method.
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Context:
+    def __init__(self, strategy: Strategy) -> None:
+        self._strategy = strategy
+
+    @property
+    def strategy(self) -> Strategy:
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, strategy: Strategy) -> None:
+        self._strategy = strategy
+
+    def do_some_business_logic(self) -> None:
+        print("Context: Sorting data using the strategy (not sure how it'll do it)")
+        result = self._strategy.do_algorithm(["a", "b", "c", "d", "e"])
+        print(",".join(result))
+
+
+class Strategy(ABC):
+    @abstractmethod
+    def do_algorithm(self, data: List):
+        pass
+
+
+class ConcreteStrategyA(Strategy):
+    def do_algorithm(self, data: List) -> List:
+        return sorted(data)
+
+
+class ConcreteStrategyB(Strategy):
+    def do_algorithm(self, data: List) -> List:
+        return sorted(data, reverse=True)
+
+
+if __name__ == "__main__":
+    context = Context(ConcreteStrategyA())
+    print("Client: Strategy is set to normal sorting.")
+    context.do_some_business_logic()
+    print()
+
+    print("Client: Strategy is set to reverse sorting.")
+    context.strategy = ConcreteStrategyB()
+    context.do_some_business_logic()
